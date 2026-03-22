162c937f4265df4fabb273bfb6b7eb446958c0ad
Santiago Cabornero <santiago.333.cabornero@gmail.com>
2026-03-22 13:52:44 -0300

    Mejora y orden de infra

--- src/infrastructure/database/crud/account_crud.py
+++ src/infrastructure/database/crud/account_crud.py
@@ -20,59 +20,7 @@ def create_account(db: Session, owner: str, initial_balance: float = 0): #db es 
 
 
 # Funcion para obtener una cuenta por su ID, devuelve el primer resultado o None si no se encuentra
-def get_account(db: Session, account_id: int):
+def get_account_by_id(db: Session, account_id: int):
     return db.query(Account).filter(Account.id == account_id).first() # Consulta la cuenta por id, devuelve el primer resultado o None
     # Query sirve para entrar en la tabla, filter para aplicar un filtro (en este caso donde el id de la tabla 
     # sea igual al id del parametro) y first para obtener el primer resultado
-
-
-
-# Transferencia
-def transfer_money(db: Session, from_account_id: int, to_account_id: int, amount: float):
-    
-    if amount <= 0:
-        raise ValueError("El monto debe ser mayor a 0") # Raise lanza un error, y detiene la ejecucion de la funcion
-
-    try:
-        # Busca las cuentas de origen y destino de la transferencia
-        # Antes:
-            # from_account = db.query(Account).filter(Account.id == from_account_id).first()
-            # to_account = db.query(Account).filter(Account.id == to_account_id).first()
-
-        #Ahora con with_for_update() para bloquear las filas de las cuentas durante la transferencia, 
-        # evitando problemas de concurrencia (E.g: que se intente transferir desde la misma cuenta al mismo tiempo)
-        from_account = (
-            db.query(Account)
-            .filter(Account.id == from_account_id)
-            .with_for_update()
-            .first()
-        )
-        to_account = (
-            db.query(Account)
-            .filter(Account.id == to_account_id)
-            .with_for_update()
-            .first()
-        )
-
-        # Si alguna de las cuentas no existe, error
-        if not from_account or not to_account:
-            raise ValueError("Cuenta no encontrada")
-        
-        #Si la cuenta de origen no tiene suficiente saldo, error
-        if from_account.balance < amount:
-            raise ValueError("Saldo insuficiente")
-
-        from_account.balance -= amount # Resta el monto de la cuenta de origen
-        to_account.balance += amount # Suma el monto a la cuenta de destino
-
-        db.commit()
-        db.refresh(from_account)
-        db.refresh(to_account)
-
-        return from_account, to_account
-
-    # Si ocurre cualquier error durante la transferencia, se hace rollback, es decir, se deshacen 
-    # los cambios realizados en la sesión, para evitar problematicas.
-    except Exception as e:
-        db.rollback()
-        raise e
